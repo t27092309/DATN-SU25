7427fea73dc006415b553b5b35a34cf898e6122e
anhphat123456789 <hunganh15610@gmail.com>
2025-05-27 18:33:10 +0700

    refactor: sắp xếp lại route API

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -9,10 +9,13 @@ use App\Http\Controllers\API\ProductVariantController;
 
 
 Route::middleware([CorsMiddleware::class])->group(function () {
+    // route admin
     Route::apiResource('categories', CategoryController::class);
-
     Route::apiResource('coupons', CouponController::class);
-
+    Route::apiResource('products', ProductController::class);
+    Route::get('product-variants/trashed', [ProductVariantController::class, 'trashed']);
+    Route::put('product-variants/restore/{id}', [ProductVariantController::class, 'restore']);
+    Route::apiResource('product-variants', ProductVariantController::class);
 
     Route::get('/hello', function () {
         return response()->json([
@@ -28,9 +31,3 @@ Route::middleware([CorsMiddleware::class])->group(function () {
 //         'message' => 'Hello from Laravel with manual CORS!',
 //     ]);
 // });
-
-Route::apiResource('products', ProductController::class);
-Route::get('product-variants/trashed', [ProductVariantController::class, 'trashed']);
-Route::put('product-variants/restore/{id}', [ProductVariantController::class, 'restore']);
-Route::apiResource('product-variants', ProductVariantController::class);
-
