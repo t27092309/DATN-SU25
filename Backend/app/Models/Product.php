4200a607e6362cb63388829e47f6e3c79b15d37c
t27092309 <truongdoxuan796@gmail.com>
2025-05-28 16:23:32 +0700

    add views to products

--- Backend/app/Models/Product.php
+++ Backend/app/Models/Product.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'slug', 'image', 'description', 'gender', 'price', 'category_id', 'brand_id' ,'views'
+    ];
+
+
+    public function images()
+{
+    return $this->hasMany(ProductImage::class);
 }
+
+public function variants()
+{
+    return $this->hasMany(ProductVariant::class);
+}
+}
+
+
