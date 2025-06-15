baa6a42852296f9fe7d1bbaf0e9b94430e6cd0a0
cngcool <cuongcool2k51@gmail.com>
2025-06-15 15:40:44 +0700

    Mô tả thay đổi

--- Frontend/src/router/client.js
+++ Frontend/src/router/client.js
@@ -1,125 +1,136 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import ClientLayout from '@/layouts/ClientLayout.vue'
-import Home from '@/views/client/Home.vue'
-import NuocHoaPage from '@/views/client/NuocHoaPage.vue'
-import NuocHoaNamPage from '@/views/client/NuocHoaNamPage.vue'
-import NuocHoaNuPage from '@/views/client/NuocHoaNuPage.vue'
-import ThuongHieuPage from '@/views/client/ThuongHieuPage.vue'
-import NuocHoaMiniPage from '@/views/client/NuocHoaMiniPage.vue'
-import NuocHoaChietPage from '@/views/client/NuocHoaChietPage.vue'
-import NuocHoaNichePage from '@/views/client/NuocHoaNichePage.vue'
-import NuocHoaUnisexPage from '@/views/client/NuocHoaUnisexPage.vue'
-import Cart from '@/views/client/Cart.vue'
-import Product from '@/views/client/Product.vue'
-import Register from '@/views/client/account/Register.vue'
-import Login from '@/views/client/account/Login.vue'
+import ClientLayout from "@/layouts/ClientLayout.vue";
+import Home from "@/views/client/Home.vue";
+import NuocHoaPage from "@/views/client/NuocHoaPage.vue";
+import NuocHoaNamPage from "@/views/client/NuocHoaNamPage.vue";
+import NuocHoaNuPage from "@/views/client/NuocHoaNuPage.vue";
+import ThuongHieuPage from "@/views/client/ThuongHieuPage.vue";
+import NuocHoaMiniPage from "@/views/client/NuocHoaMiniPage.vue";
+import NuocHoaChietPage from "@/views/client/NuocHoaChietPage.vue";
+import NuocHoaNichePage from "@/views/client/NuocHoaNichePage.vue";
+import NuocHoaUnisexPage from "@/views/client/NuocHoaUnisexPage.vue";
+import Cart from "@/views/client/Cart.vue";
+import Product from "@/views/client/Product.vue";
+import Register from "@/views/client/account/Register.vue";
+import Login from "@/views/client/account/Login.vue";
 
 //-----User--------
-import UserLayout from '@/layouts/UserLayout.vue'
-import Profile from '@/views/client/user/Profile.vue'
-import Address from '@/views/client/user/Address.vue';
-import Bank from '@/views/client/user/Bank.vue';
-import Orders from '@/views/client/user/Orders.vue';
+import UserLayout from "@/layouts/UserLayout.vue";
+import Profile from "@/views/client/user/Profile.vue";
+import Address from "@/views/client/user/Address.vue";
+import Bank from "@/views/client/user/Bank.vue";
+import Orders from "@/views/client/user/Orders.vue";
+import CategoryProducts from "@/views/client/CategoryProducts.vue";
+import ProductDetail from "@/views/client/Product.vue";
 export default [
   {
-    path: '/',
+    path: "/",
     component: ClientLayout,
     children: [
       {
-        path: '',
-        name: 'Home',
+        path: "",
+        name: "Home",
         component: Home,
       },
       {
-        path: 'nuoc-hoa',
-        name: 'NuocHoa',
+        path: "nuoc-hoa",
+        name: "NuocHoa",
         component: NuocHoaPage,
       },
       {
-        path: 'nuoc-hoa-nam',
-        name: 'NuocHoaNam',
+        path: "nuoc-hoa-nam",
+        name: "NuocHoaNam",
         component: NuocHoaNamPage,
       },
       {
-        path: 'nuoc-hoa-nu',
-        name: 'NuocHoaNu',
+        path: "nuoc-hoa-nu",
+        name: "NuocHoaNu",
         component: NuocHoaNuPage,
       },
       {
-        path: 'thuong-hieu',
-        name: 'ThuongHieu',
+        path: "thuong-hieu",
+        name: "ThuongHieu",
         component: ThuongHieuPage,
       },
       {
-        path: 'nuoc-hoa-mini',
-        name: 'NuocHoaMini',
+        path: "nuoc-hoa-mini",
+        name: "NuocHoaMini",
         component: NuocHoaMiniPage,
       },
       {
-        path: 'nuoc-hoa-chiet',
-        name: 'NuocHoaChiet',
+        path: "nuoc-hoa-chiet",
+        name: "NuocHoaChiet",
         component: NuocHoaChietPage,
       },
       {
-        path: 'nuoc-hoa-unisex',
-        name: 'NuocHoaUnisex',
+        path: "nuoc-hoa-unisex",
+        name: "NuocHoaUnisex",
         component: NuocHoaUnisexPage,
       },
       {
-        path: 'nuoc-hoa-niche',
-        name: 'NuocHoaNiche',
+        path: "nuoc-hoa-niche",
+        name: "NuocHoaNiche",
         component: NuocHoaNichePage,
       },
       {
-        path: 'gio-hang',
-        name: 'GioHang',
+        path: "gio-hang",
+        name: "GioHang",
         component: Cart,
       },
       {
-        path: 'san-pham',
-        name: 'SanPham',
-        component: Product,
-      },
-      {
-        path: 'dang-ky',
-        name: 'DangKy',
+        path: "dang-ky",
+        name: "DangKy",
         component: Register,
       },
       {
-        path: 'dang-nhap',
-        name: 'DangNhap',
+        path: "dang-nhap",
+        name: "DangNhap",
         component: Login,
       },
+      {
+        path: "/category/:categoryName",
+        name: "CategoryProducts",
+        component: CategoryProducts,
+        props: true,
+      },
+
+      {
+        path: "/product/:productId",
+        name: "ProductDetail",
+        component: ProductDetail,
+        props: true,
+      },
+
       //----User Path-----
       {
-        path: '/tai-khoan',
-        name: 'User',
+        path: "/tai-khoan",
+        name: "User",
         component: UserLayout,
-        redirect: { name: 'Profile' },
+        redirect: { name: "Profile" },
         children: [
           {
-            path: 'ho-so', // Path will be /user/profile
-            name: 'Profile',
+            path: "ho-so", // Path will be /user/profile
+            name: "Profile",
             component: Profile, // A component for detailed profile
           },
           {
-            path: 'dia-chi', // Path will be /user/profile
-            name: 'Address',
+            path: "dia-chi", // Path will be /user/profile
+            name: "Address",
             component: Address, // A component for detailed profile
           },
           {
-            path: 'ngan-hang', // Path will be /user/profile
-            name: 'Bank',
+            path: "ngan-hang", // Path will be /user/profile
+            name: "Bank",
             component: Bank, // A component for detailed profile
           },
           {
-            path: 'don-hang', // Path will be /user/profile
-            name: 'Orders',
+            path: "don-hang", // Path will be /user/profile
+            name: "Orders",
             component: Orders, // A component for detailed profile
           },
-        ]
-      }
-    ]
-  }
-]
+        ],
+      },
+    ],
+  },
+];
