755225ae1614b9dea74e0f2619baba4363f43c78
vunhols <hoangfvnx@gmail.com>
2025-06-27 18:11:57 +0700

    thanh toán cho client

--- Backend/app/Models/OrderItem.php
+++ Backend/app/Models/OrderItem.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+    'order_id',
+    'product_variant_id',
+    'quantity',
+    'price_each',
+    'variant_sku',
+    'variant_name',
+    'variant_status',
+    'variant_description',
+];
 }
