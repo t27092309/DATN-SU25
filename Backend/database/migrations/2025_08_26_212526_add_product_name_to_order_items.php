788bb7f02fddcfd838f693088e6856c64695a2bc
vunhols <hoangfvnx@gmail.com>
2025-08-26 21:26:47 +0700

    thêm product_name vào bảng order_items

--- Backend/database/migrations/2025_08_26_212526_add_product_name_to_order_items.php
+++ Backend/database/migrations/2025_08_26_212526_add_product_name_to_order_items.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
-            $table->string('product_name')->after('total_price');
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->string('product_name')->after('order_id');
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('order_items', function (Blueprint $table) {
             $table->dropColumn('product_name');
         });
     }
